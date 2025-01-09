bf5bfd07876aa215cf4b157bf48cb3494c33c501
Dawid Sulicki <filipgarbacz457@gmail.com>
2025-01-09 20:42:20 +0100

    dodano login

--- saperproject/saperproject/urls.py
+++ saperproject/saperproject/urls.py
@@ -6,4 +6,6 @@ from projectwebsite.views import home, saper
 urlpatterns = [
     path('admin/', admin.site.urls),
      path('', include('projectwebsite.urls')),
+    path('users/', include('django.contrib.auth.urls')),
+    path('users/', include('users.urls')),
 ]
